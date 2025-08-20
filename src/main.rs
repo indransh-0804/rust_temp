2c8badc4f373bb2e801065fa908cac9a76562185
Indransh Harikesh Sharma <indransh.0804.sharma@gmail.com>
2025-08-20 06:48:15 +0530

    init src dir

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+//! # My Project
+//! - Author: Indransh Sharma <indransh.0804.sharma@gmail.com>                
+//! - Created: 2025-08-20 06:45:08                               
+
+#![deny(clippy::correctness, clippy::perf, clippy::complexity)]
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+	
+	Ok(())
+}
+
